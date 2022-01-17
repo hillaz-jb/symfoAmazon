c7e84dc82c9b0428d8bb94ec2bc1002241290dcc
Mas Galfane <mouzdaher@outlook.fr>
2022-01-17 14:31:16 +0100

    entity

---
+++ src/Entity/Adress.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AdressRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AdressRepository::class)]
+class Adress
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $adress1;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $adress2;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $country;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $city;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $zip_code;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAdress1(): ?string
+    {
+        return $this->adress1;
+    }
+
+    public function setAdress1(string $adress1): self
+    {
+        $this->adress1 = $adress1;
+
+        return $this;
+    }
+
+    public function getAdress2(): ?string
+    {
+        return $this->adress2;
+    }
+
+    public function setAdress2(string $adress2): self
+    {
+        $this->adress2 = $adress2;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getZipCode(): ?string
+    {
+        return $this->zip_code;
+    }
+
+    public function setZipCode(string $zip_code): self
+    {
+        $this->zip_code = $zip_code;
+
+        return $this;
+    }
+}
